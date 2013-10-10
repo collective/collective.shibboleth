3b979a01db6e56dfd53c09bff62ee4ccef43101d
David\ Beitey <david@davidjb.com>
2013-10-10 11:21:32 +1000

    Add pluggable login as dependency

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setup(name='collective.aaf',
       install_requires=[
           'setuptools',
           'collective.monkeypatcher',
+          'collective.pluggablelogin',
           'Products.AutoUserMakerPASPlugin>=0.8b2dev',
           # -*- Extra requirements: -*-
       ],
