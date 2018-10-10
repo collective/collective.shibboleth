91d576a2895214920150b76dc1d2e193d44cd424
Timo Stollenwerk <stollenwerk@kitconcept.com>
2018-10-10 12:06:31 +0200

    Fix tests. There is no c.s.portlets package. Seems the test never actually ran.

--- src/collective/shibboleth/tests/base_shibbolethloginportlet.py
+++ src/collective/shibboleth/tests/base_shibbolethloginportlet.py
@@ -17,21 +17,21 @@ def setup_product():
     # Load the ZCML configuration for this package and its dependencies
 
     fiveconfigure.debug_mode = True
-    import collective.shibboleth.portlets
-    zcml.load_config('configure.zcml', collective.shibboleth.portlets)
+    import collective.shibboleth
+    zcml.load_config('configure.zcml', collective.shibboleth)
     fiveconfigure.debug_mode = False
 
     # We need to tell the testing framework that these products
     # should be available. This can't happen until after we have loaded
     # the ZCML.
 
-    ztc.installPackage('collective.shibboleth.portlets')
+    ztc.installPackage('collective.shibboleth')
 
 # The order here is important: We first call the deferred function and then
 # let PloneTestCase install it during Plone site setup
 
 setup_product()
-ptc.setupPloneSite(products=['collective.shibboleth.portlets'])
+ptc.setupPloneSite(products=['collective.shibboleth'])
 
 
 class TestCase(ptc.PloneTestCase):
